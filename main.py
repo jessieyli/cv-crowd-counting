53531091c84253b1ed8e1b64dbd8c188aaa6b5d9
jessieyli <50643746+jessieyli@users.noreply.github.com>
2021-04-14 01:59:55 -0400

    naive yolo implementation

--- main.py
+++ main.py
@@ -1,26 +1,31 @@
 import argparse
 import cv2
+import numpy as np
+from yolo3 import YoloV3, load_pretrained_weights, weights_download
 
 # detect people and make box
+
+
 def detect(frame):
-    
+
     # bounding_box_cordinates, weights =  HOGCV.detectMultiScale(frame, winStride = (4, 4), padding = (8, 8), scale = 1.03)
-    
+
     # person = 1
     # for x,y,w,h in bounding_box_cordinates:
     #     cv2.rectangle(frame, (x,y), (x+w,y+h), (0,255,0), 2)
     #     cv2.putText(frame, f'person {person}', (x,y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,255), 1)
     #     person += 1
-    
+
     # cv2.putText(frame, 'Status : Detecting ', (40,40), cv2.FONT_HERSHEY_DUPLEX, 0.8, (255,0,0), 2)
     # cv2.putText(frame, f'Total Persons : {person-1}', (40,70), cv2.FONT_HERSHEY_DUPLEX, 0.8, (255,0,0), 2)
-    
+
     # TODO: IMPLEMENT PEOPLE DETECTOR
     cv2.imshow('output', frame)
 
     return frame
 
-def useCamera(writer):   
+
+def useCamera(writer):
     video = cv2.VideoCapture(0)
     print('Detecting people...')
     while True:
@@ -33,6 +38,7 @@ def useCamera(writer):
             break
     video.release()
     cv2.destroyAllWindows()
+
 
 def useVideo(path, writer):
     video = cv2.VideoCapture(path)
@@ -42,24 +48,25 @@ def useVideo(path, writer):
         return
     print('Detecting people...')
     while video.isOpened():
-        #check is True if reading was successful 
-        check, frame =  video.read()
+        # check is True if reading was successful
+        check, frame = video.read()
         if check:
             # frame = imutils.resize(frame , width=min(800,frame.shape[1]))
             frame = detect(frame)
-            
+
             if writer is not None:
                 writer.write(frame)
-            
+
             key = cv2.waitKey(1)
-            if key== ord('q'):
+            if key == ord('q'):
                 break
         else:
             break
     video.release()
     cv2.destroyAllWindows()
 
-def useCamera(writer):   
+
+def useCamera(writer):
     video = cv2.VideoCapture(0)
     print('Detecting people...')
     while True:
@@ -69,14 +76,15 @@ def useCamera(writer):
             writer.write(frame)
         key = cv2.waitKey(1)
         if key == ord('q'):
-                break
+            break
     video.release()
     cv2.destroyAllWindows()
+
 
 def useImage(path, output_path):
     image = cv2.imread(path)
 
-    # image = imutils.resize(image, width = min(800, image.shape[1])) 
+    # image = imutils.resize(image, width = min(800, image.shape[1]))
 
     result_image = detect(image)
 
@@ -87,20 +95,76 @@ def useImage(path, output_path):
     cv2.destroyAllWindows()
 
 
+def useImageYolo(path, output_path):
+    weights_download()
+
+    yolo = YoloV3()
+    load_pretrained_weights(yolo, 'models/yolov3.weights')
+
+    print("reading image")
+    image = cv2.imread(path)
+
+    img = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    img = cv2.resize(img, (320, 320))
+    img = img.astype(np.float32)
+    img = np.expand_dims(img, 0)
+    img = img / 255
+
+    print("running yolo")
+    boxes, scores, classes, nums = yolo(img)
+    count = 0
+    display_boxes = []
+    display_scores = []
+    for i in range(nums[0]):
+        if int(classes[0][i] == 0):
+            count += 1
+            display_boxes.append(boxes[0][i])
+            display_scores.append(scores[0][i])
+
+    wh = np.flip(img.shape[0:2])
+    for i in range(len(display_boxes)):
+        x1y1 = tuple((np.array(display_boxes[i][0:2]) * wh).astype(np.int32))
+        x2y2 = tuple((np.array(display_boxes[i][2:4]) * wh).astype(np.int32))
+        img = cv2.rectangle(img, x1y1, x2y2, (255, 0, 0), 2)
+        img = cv2.putText(img, f'person {display_scores[i]}',
+                          x1y1, cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (0, 0, 255), 2)
+
+    cv2.putText(img, 'Status : Detecting ', (40, 40),
+                cv2.FONT_HERSHEY_DUPLEX, 0.8, (255, 0, 0), 2)
+    cv2.putText(img, f'Total Persons : {count}', (40, 70),
+                cv2.FONT_HERSHEY_DUPLEX, 0.8, (255, 0, 0), 2)
+
+    print(img[0].shape)
+    img = cv2.cvtColor(img[0], cv2.COLOR_RGB2BGR)
+
+    # cv2.imshow('output', img)
+
+    if output_path is not None:
+
+        cv2.imwrite(output_path, img)
+
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
 
 if __name__ == "__main__":
     arg_parse = argparse.ArgumentParser()
-    arg_parse.add_argument("-v", "--video", default=None, help="video file path")
-    arg_parse.add_argument("-i", "--image", default=None, help="image file path")
-    arg_parse.add_argument("-w", "--webcam", default=False, help="Set true to use webcam")
+    arg_parse.add_argument("-v", "--video", default=None,
+                           help="video file path")
+    arg_parse.add_argument("-i", "--image", default=None,
+                           help="image file path")
+    arg_parse.add_argument("-w", "--webcam", default=False,
+                           help="Set true to use webcam")
     arg_parse.add_argument("-o", "--output", type=str, help="output path")
+    arg_parse.add_argument("-y", "--yolo", action="store_true", help="use YOLOv3")
     args = vars(arg_parse.parse_args())
 
     # TODO: WRITER DOES NOT WORK FOR VIDS
     if args['output'] is not None and args['image'] is None:
-        writer = cv2.VideoWriter(args['output'],cv2.VideoWriter_fourcc(*'MJPG'), 10, (600,600))
+        writer = cv2.VideoWriter(
+            args['output'], cv2.VideoWriter_fourcc(*'MJPG'), 10, (600, 600))
     else:
-      writer = None
+        writer = None
 
     if args['webcam']:
         print('USING WEBCAM')
@@ -108,6 +172,9 @@ if __name__ == "__main__":
     elif args['video'] is not None:
         print('USING VIDEO')
         useVideo(args['video'], writer)
+    elif args['image'] is not None and args['yolo']:
+        print('USING IMAGE W YOLO')
+        useImageYolo(args['image'], args['output'])
     elif args['image'] is not None:
         print('USING IMAGE')
         useImage(args['image'], args['output'])
